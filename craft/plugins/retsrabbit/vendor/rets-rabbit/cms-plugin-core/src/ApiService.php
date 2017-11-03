363216c0633e77e5c39d6226ae9aa64c84d87417
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-03 15:52:19 -0400

    update endpoint for testing purposes

--- craft/plugins/retsrabbit/vendor/rets-rabbit/cms-plugin-core/src/ApiService.php
+++ craft/plugins/retsrabbit/vendor/rets-rabbit/cms-plugin-core/src/ApiService.php
@@ -34,7 +34,7 @@ class ApiService
      *
      * @var string
      */
-    private $apiOauthEndpoint = 'oauth/access_token';
+    private $apiOauthEndpoint = 'api/oauth/access_token';
 
     /**
      * Built API endpoint.
