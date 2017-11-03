8fcbfb66e541dfc6488db0d131a0ff011bf6526f
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-03 09:37:55 -0400

    began working on properties service

---
+++ craft/plugins/retsrabbit/services/RetsRabbit_PropertiesService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Craft;
+
+use RetsRabbit\ApiService;
+use RetsRabbit\Bridges\CraftBridge;
+use RetsRabbit\Resources\PropertiesResource;
+
+class RetsRabbit_PropertiesService extends BaseApplicationComponent
+{
+	/**
+	 * The api service from the core RR library
+	 * 
+	 * @var ApiService
+	 */
+	private $api;
+
+	/**
+	 * The properties resource endpoint
+	 * 
+	 * @var PropertiesResource
+	 */
+	private $resource;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$bridge = new CraftBridge;
+		$bridge->setTokenFetcher(function () {
+			return craft()->retsRabbit_cache->get('access_token', true);
+		});
+		$this->api = new ApiService($bridge);
+		$this->resource = new PropertiesResource($this->api);
+	}
+
+	/**
+	 * @param  array
+	 * @return array
+	 */
+	public function search($params = array())
+	{
+		$res = $this->resource->search($params);
+
+		return $res;
+	}
+
+	/**
+	 * @param  string
+	 * @return array
+	 */
+	public function find($id = '')
+	{
+		$res = $this->resource->single($id);
+
+		return $res;
+	}
+}
