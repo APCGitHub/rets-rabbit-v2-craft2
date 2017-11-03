7ebe5f3bbecd20c1307b33c939834feef420ec54
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-03 16:48:12 -0400

    started working on the search controller

--- craft/plugins/retsrabbit/controllers/RetsRabbit_PropertiesController.php
+++ craft/plugins/retsrabbit/controllers/RetsRabbit_PropertiesController.php
@@ -2,10 +2,17 @@
 
 namespace Craft;
 
-class RetsRabbit_PropertiesController extends BaseControler
+class RetsRabbit_PropertiesController extends BaseController
 {
+	protected $allowAnonymous = array('actionSearch');
+
 	public function actionSearch()
 	{
-		
+		$this->requirePostRequest();
+
+		$data = craft()->request->getPost();
+		$resoParams = craft()->retsRabbit_form->toReso($data);
+
+		$this->returnJson($resoParams);
 	}
 }
