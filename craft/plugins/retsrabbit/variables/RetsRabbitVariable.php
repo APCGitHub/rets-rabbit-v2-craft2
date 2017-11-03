f473b803831bd2c84a7cbebd8ef70dd9ec317b90
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-03 09:38:19 -0400

    rr variable uses properties service now

--- craft/plugins/retsrabbit/variables/RetsRabbitVariable.php
+++ craft/plugins/retsrabbit/variables/RetsRabbitVariable.php
@@ -4,8 +4,33 @@ namespace Craft;
 
 class RetsRabbitVariable
 {
-	public function resources()
+	/**
+	 * @param  array
+	 * @return array
+	 */
+	public function properties($params = array())
 	{
-		
+		if(is_null($id) || empty($params)) {
+			throw new \Exception("You must pass in an associative array of params.");
+		}
+
+		$results = craft()->retsRabbit_properties->search($params);
+
+		return $results;
+	}
+
+	/**
+	 * @param  string
+	 * @return array
+	 */
+	public function property($id = '')
+	{
+		if(is_null($id) || empty($id)) {
+			throw new \Exception("You must pass in a valid listing ID");
+		}
+
+		$listing = craft()->retsRabbit_properties->find($id);
+
+		return $listing;
 	}
 }
