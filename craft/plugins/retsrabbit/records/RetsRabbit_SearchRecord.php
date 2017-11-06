5181344581bd743edc525ca1a1091d50d79f7bdc
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-06 11:14:41 -0500

    fixed missing comma

--- craft/plugins/retsrabbit/records/RetsRabbit_SearchRecord.php
+++ craft/plugins/retsrabbit/records/RetsRabbit_SearchRecord.php
@@ -19,8 +19,7 @@ class RetsRabbit_SearchRecord extends BaseRecord
 	{
 		return array(
 			'type' => array(AttributeType::String, 'required' => true, 'default' => 'property'),
-			'params' => array(AttributeType::String, 'column' => ColumnType::Text 'required' => true),
-			'created_at' => array(AttributeType::DateTime, 'required' => true),
+			'params' => array(AttributeType::String, 'column' => ColumnType::Text, 'required' => true)
 		);
 	}
 }
