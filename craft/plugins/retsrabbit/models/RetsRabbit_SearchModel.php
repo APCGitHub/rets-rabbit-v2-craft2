8e1408919e96fa16110043bea25dcf783bb2b511
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-06 11:06:55 -0500

    created a new search model

---
+++ craft/plugins/retsrabbit/models/RetsRabbit_SearchModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Craft;
+
+class RetsRabbit_SearchModel extends BaseModel
+{
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function defineAttributes()
+	{
+		return array(
+			'id' => AttributeType::Number,
+			'type' => AttributeType::String,
+			'params' => AttributeType::String,
+			'created_at' => AttributeType::DateTime,
+		);
+	}
+}
