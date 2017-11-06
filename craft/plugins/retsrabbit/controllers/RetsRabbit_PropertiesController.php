81316f9d010a61ddc8b1043b5a69f7bf636d22df
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-06 11:06:44 -0500

    implement new services to handle searching

--- craft/plugins/retsrabbit/controllers/RetsRabbit_PropertiesController.php
+++ craft/plugins/retsrabbit/controllers/RetsRabbit_PropertiesController.php
@@ -4,15 +4,28 @@ namespace Craft;
 
 class RetsRabbit_PropertiesController extends BaseController
 {
+	/**
+	 * Allow these endpoints to be hit by anonymous users
+	 * @var array
+	 */
 	protected $allowAnonymous = array('actionSearch');
 
+	/**
+	 * Handle a POST search by saving params into the DB and redirecting
+	 * to the search results page.
+	 * 
+	 * @return mixed
+	 */
 	public function actionSearch()
 	{
 		$this->requirePostRequest();
 
 		$data = craft()->request->getPost();
-		$resoParams = craft()->retsRabbit_form->toReso($data);
+		$resoParams = craft()->retsRabbit_forms->toReso($data);
+		$search = craft()->retsRabbit_searches->newPropertySearch(array(
+			'params' => $resoParams
+		));
 
-		$this->returnJson($resoParams);
+		$this->redirectToPostedUrl(array('searchId' => $search->id));
 	}
 }
