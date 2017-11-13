8c2434823bd9c7af8c26d4e4406a66a344af2f6d
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-13 11:58:45 -0500

    fix include data

--- craft/plugins/retsrabbit/transforms/PropertyTransformer.php
+++ craft/plugins/retsrabbit/transforms/PropertyTransformer.php
@@ -6,28 +6,28 @@ use League\Fractal\TransformerAbstract;
 
 class PropertyTransformer extends TransformerAbstract
 {
-	/**
-	 * @var array
-	 */
-	protected $availableIncludes = array('photos');
+    /**
+     * @var array
+     */
+    protected $availableIncludes = array('photos');
 
-	/**
-	 * @var array
-	 */
+    /**
+     * @var array
+     */
     protected $defaultIncludes = array('photos');
 
     /**
      * @param  $listing array
      * @return array|null
      */
-	public function transform($listing = array())
-	{
-		$data = $listing;
+    public function transform($listing = array())
+    {
+        $data = $listing;
         $data['has_photos'] = false;
         $data['total_photos'] = 0;
 
-        if(isset($data['photos'])) {
-            $count = sizeof($data['photos']);
+        if(isset($data['listing']) && isset($data['listing']['photos'])) {
+            $count = sizeof($data['listing']['photos']);
             if($count) {
                 $data['has_photos'] = true;
                 $data['total_photos'] = $count;
@@ -35,17 +35,17 @@ class PropertyTransformer extends TransformerAbstract
         }
 
         return $data;
-	}
+    }
 
-	/**
-	 * @param  array
-	 * @return array
-	 */
-	public function includePhotos($listing = array())
+    /**
+     * @param  array
+     * @return array
+     */
+    public function includePhotos($listing = array())
     {   $photos = array();
 
-        if(isset($listing['photos']))
-            $photos = $listing['photos'];
+        if(isset($listing['listing']) && isset($listing['listing']['photos']))
+            $photos = $listing['listing']['photos'];
 
         return $this->collection($photos, new PhotoTransformer);
     }
