0c04d010c2f3eed770e99cee629554839e4d2c96
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-13 16:01:25 -0500

    remove photo transformer for now

--- craft/plugins/retsrabbit/transforms/PropertyTransformer.php
+++ craft/plugins/retsrabbit/transforms/PropertyTransformer.php
@@ -7,46 +7,23 @@ use League\Fractal\TransformerAbstract;
 class PropertyTransformer extends TransformerAbstract
 {
     /**
-     * @var array
-     */
-    protected $availableIncludes = array('photos');
-
-    /**
-     * @var array
-     */
-    protected $defaultIncludes = array('photos');
-
-    /**
      * @param  $listing array
      * @return array|null
      */
     public function transform($listing = array())
     {
         $data = $listing;
-        $data['has_photos'] = false;
-        $data['total_photos'] = 0;
+        $data['hasPhotos'] = false;
+        $data['totalPhotos'] = 0;
 
         if(isset($data['listing']) && isset($data['listing']['photos'])) {
             $count = sizeof($data['listing']['photos']);
             if($count) {
-                $data['has_photos'] = true;
-                $data['total_photos'] = $count;
+                $data['hasPhotos'] = true;
+                $data['totalPhotos'] = $count;
             }
         }
 
         return $data;
-    }
-
-    /**
-     * @param  array
-     * @return array
-     */
-    public function includePhotos($listing = array())
-    {   $photos = array();
-
-        if(isset($listing['listing']) && isset($listing['listing']['photos']))
-            $photos = $listing['listing']['photos'];
-
-        return $this->collection($photos, new PhotoTransformer);
     }
 }
