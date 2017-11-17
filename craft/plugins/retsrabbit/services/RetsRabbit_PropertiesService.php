09a319708c4a84952e949df6a491e453a60bb51e
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-17 16:10:52 -0500

    Retry api call if fails

--- craft/plugins/retsrabbit/services/RetsRabbit_PropertiesService.php
+++ craft/plugins/retsrabbit/services/RetsRabbit_PropertiesService.php
@@ -27,12 +27,24 @@ class RetsRabbit_PropertiesService extends BaseApplicationComponent
 	 */
 	public function __construct()
 	{
+		$settings = craft()->plugins->getPlugin('retsRabbit')->getSettings();
 		$bridge = new CraftBridge;
+
+		//Set the token fetcher function so the core lib can grab tokens
+		//from cache on the plugin's behalf
 		$bridge->setTokenFetcher(function () {
 			return craft()->retsRabbit_cache->get('access_token', true);
 		});
+
+		//Load the Craft Bridge into the ApiService
 		$this->api = new ApiService($bridge);
-		$this->api->overrideBaseApiEndpoint('https://stage.retsrabbit.com');
+
+		//Allow developer to override base endpoint
+		if($settings->apiEndpoint) {
+			$this->api->overrideBaseApiEndpoint($settings->apiEndpoint);
+		}
+
+		//Instantiate the PropertiesResource
 		$this->resource = new PropertiesResource($this->api);
 	}
 
@@ -43,6 +55,24 @@ class RetsRabbit_PropertiesService extends BaseApplicationComponent
 	public function search($params = array())
 	{
 		$res = $this->resource->search($params);
+
+		if($res->didFail()) {
+			$contents = $res->getResponse();
+
+			if(isset($contents['error']) && isset($contents['error']['code'])) {
+				RetsRabbitPlugin::log('A permission error occurred.', LogLevel::Error);
+				
+				$code = $contents['error']['code'];
+
+				if($code == 'permission') {
+					$success = craft()->retsRabbit_tokens->refresh();
+
+					if(!is_null($success)) {
+						$res = $this->resource->search($params);
+					}
+				}
+			}
+		}
 
 		return $res;
 	}
